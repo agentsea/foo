446b9c076f25bc7e41b7d6866e45d356b6bb2b1a
Patrick Barker <patrickbarkerco@gmail.com>
2025-07-20 14:43:40 -0600

    fix rclone

--- foo/qwen_server.py
+++ foo/qwen_server.py
@@ -1210,7 +1210,7 @@ def QwenVLServer(
     platform: str = "runpod",
     accelerators: List[str] = ["1:A100_SXM"],
     model: str = "agentsea/Qwen2.5-VL-32B-Instruct-CARL-Gflights4",
-    image: str = "public.ecr.aws/d8i6n0n1/orign/unsloth-server:144fe72",  # "public.ecr.aws/d8i6n0n1/orign/unsloth-server:8b0ee04",  # "us-docker.pkg.dev/agentsea-dev/orign/unsloth-infer:latest"
+    image: str = "public.ecr.aws/d8i6n0n1/orign/unsloth-server:825d581",  # "public.ecr.aws/d8i6n0n1/orign/unsloth-server:8b0ee04",  # "us-docker.pkg.dev/agentsea-dev/orign/unsloth-infer:latest"
     namespace: Optional[str] = None,
     env: Optional[List[V1EnvVar]] = None,
     config: Optional[NebuGlobalConfig] = None,
